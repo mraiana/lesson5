d9a383defbea541e5defe6c0f9669599fafefdcf
mraiana <semeteevna0602@gmail.com>
2025-11-15 10:56:13 +0600

    nextapp

---
+++ app/info/page.tsx
@@ -0,0 +1,8 @@
+export default function Info(){
+  return (
+    <div>
+    <h1>Info page</h1>
+    <p>Some information here</p>
+    </div>
+  );
+};
---
+++ app/nav/page.tsx
@@ -0,0 +1,8 @@
+export default function Nav(){
+  return (
+    <div>
+    <h1>Navigation page</h1>
+    <p>Some information here</p>
+    </div>
+  );
+};
